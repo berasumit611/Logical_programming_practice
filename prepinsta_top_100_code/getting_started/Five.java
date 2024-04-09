03480bad9fba8818dbcab1ccf29c1f98579b4179
Sumit Bera <berasumit611@gmail.com>
2024-04-09 22:01:22 +0530

    five added

--- prepinsta_top_100_code/getting_started/Five.java
+++ prepinsta_top_100_code/getting_started/Five.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
 public class Five {
-    
+
+    public static void main(String[] args) {
+        Scanner r=new Scanner(System.in);
+        System.out.print("Enter starting number1 => ");
+        int a=r.nextInt();
+        System.out.print("Enter starting number2 => ");
+        int b=r.nextInt();
+        int sum=0;
+        for(int i=a;i<=b;i++){
+            sum+=i;
+                }
+            System.out.println("Sum ==> "+sum);
+    }
+   
+
+
 }
