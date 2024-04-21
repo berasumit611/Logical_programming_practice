59a67dfb04d382344b27c1fd2c540f8b770c63c6
Sumit Bera <berasumit611@gmail.com>
2024-04-21 20:27:57 +0530

    8TH

---
+++ prepinsta_top_100_code/getting_started/th8.java
@@ -0,0 +1,18 @@
+//checking leap-year
+import java.util.Scanner;
+class th8 {
+    public static void main(String[] args){
+        Scanner r=new Scanner(System.in);
+        System.out.print("Enter year=> ");
+        int year=r.nextInt();
+        if(year%400==0){
+            System.out.println(year+" is Leap year");
+        }
+        else if(year%100!=0 && year%4==0){
+            System.out.println(year+" is Leap year");
+        }
+        else{
+            System.out.println("Not a leap year");
+        }
+    }
+}
